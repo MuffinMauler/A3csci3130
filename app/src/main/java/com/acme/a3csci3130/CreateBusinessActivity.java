264d80e64d9271d696e95527afd1ccb8e073b08a
MuffinMauler <NathanRobertson@dal.ca>
2017-07-09 20:53:15 -0300

    [REFACTOR] Clarity and consistency

--- app/src/main/java/com/acme/a3csci3130/CreateBusinessActivity.java
+++ app/src/main/java/com/acme/a3csci3130/CreateBusinessActivity.java
@@ -8,18 +8,18 @@ import android.widget.EditText;
 
 public class CreateBusinessActivity extends Activity {
 
-    private Button submitButton;
+    private Button mSubmitButton;
     private EditText mNameField, mPrimaryField, mAddressField, mLocationField;
     private MyApplicationData mAppState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_business_acitivity);
+        setContentView(R.layout.activity_create_business_activity);
         //Get the app wide shared variables
         mAppState = ((MyApplicationData) getApplicationContext());
 
-        submitButton    = (Button) findViewById(R.id.submitButton);
+        mSubmitButton    = (Button) findViewById(R.id.submitButton);
         mNameField      = (EditText) findViewById(R.id.name);
         mPrimaryField   = (EditText) findViewById(R.id.primary);
         mAddressField   = (EditText) findViewById(R.id.address);
